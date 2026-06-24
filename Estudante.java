61138a0c2a7d41555b4cf7db1ebe84c908d00c39
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-06-23 21:40:21 -0300

    Adiciona matricula do estudante

---
+++ Estudante.java
@@ -0,0 +1,10 @@
+public class Estudante {
+    String nome;
+    int matricula;
+
+    public Estudante(String nome, int matricula) {
+    this.nome = nome;
+    this.matricula = matricula; 
+    }
+    
+}
