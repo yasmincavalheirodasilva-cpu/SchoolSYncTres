1cad05241fda36882aa045c367f6bb162f897e89
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-06-30 21:33:52 -0300

    adiciona classe principal

--- Principal.java
+++ Principal.java
@@ -1,3 +1,36 @@
 public class Principal {
-     
+
+    public static void main(String[] args) {
+
+        Estudante[] alunos = {
+            new Estudante("João", 1, true),
+            new Estudante("Yasmin", 2, true),
+            new Estudante("GAby", 3, false)
+        };
+
+        Boletim[] boletins = {
+            new Boletim(8, 9),
+            new Boletim(4, 5),
+            new Boletim(9, 8)
+        };
+
+        Frequencia[] frequencias = {
+            new Frequencia(2),
+            new Frequencia(1),
+            new Frequencia(0)
+        };
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("Aluno: " + alunos[i].nome);
+
+            if (alunos[i].mensalidadeDia) {
+                System.out.println("Média: " + boletins[i].CalcularMedia());
+                System.out.println("Faltas: " + frequencias[i].mostraFaltas());
+            } else {
+                System.out.println("Aviso: Dados retidos por pendência financeira");
+            }
+
+            System.out.println();
+        }
+    }
 }
