3f4e995620dc3560610d3455a5e82fb6b8936f0d
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-06-30 21:14:15 -0300

    adicina alteração (boolean)

--- Estudante.java
+++ Estudante.java
@@ -1,10 +1,11 @@
 public class Estudante {
     String nome;
     int matricula;
+    boolean mensalidadeDia;
 
-    public Estudante(String nome, int matricula) {
-    this.nome = nome;
-    this.matricula = matricula; 
+    public Estudante(String nome, int matricula, boolean mensalidadeDia) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.mensalidadeDia = mensalidadeDia;
     }
-    
 }
